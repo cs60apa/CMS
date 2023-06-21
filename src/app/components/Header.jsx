4d81fb47e966b30c262732e37240c50b3513f464
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-06-21 22:00:06 +0300

    updates

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,35 +1,35 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
-import { BiSearchAlt, BiToggleLeft } from 'react-icons/bi';
-import {CgProfile} from 'react-icons/cg';
+import {CiLight, CiDark, CiSearch} from "react-icons/Ci";
+import {IoIosPerson} from 'react-icons/Io';
 import { useTheme } from "next-themes";
 
+
 export default function Header() {
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme;
+    const { theme, setTheme } = useTheme();
   return (
     <div className="relative border-b-4 border-slate-700 mb-10 z-1 ">
-        <div className="flex-start">
+        <div className="flex-start ml-2">
         <p>
           <img src="https://devcircleafrica.com/wp-content/uploads/2022/07/cropped-logo-color.png" width={250} height={180} alt="logo"
           />
         </p>
         </div>
-        <div className="grid grid-cols-4 gap-4 content-center mr-5 absolute top-3 right-0 flex justify-end">
-            <div className="mr-5">
+        <div className="grid grid-cols-4 gap-4 mt-3 mr-5 absolute top-3 right-0 flex justify-end">
+            <div className="mr-2">
             <Link href="/">
-              <CgProfile size={30}/>
+              <IoIosPerson size={27}/>
             </Link>
             </div>
             <div>
-            <button className="mr-5" onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")} >
-              <BiToggleLeft size={30}/>
+            <button className="mr-2" onClick={()=> setTheme( theme === "dark"? "light": "dark" )}>
+                { theme === "dark" ? <CiLight size={27}/>: <CiDark size={27}/> }
             </button>
             </div>
             <div>
             <Link href="/">
-              <BiSearchAlt size={30}/>
+              <CiSearch size={27}/>
             </Link>
             </div>
         </div>

94e13ca01721ba4efc8778c28d6c13e0fd31c3a9
marksikaundi <emmanuelmark117@gmail.com>
2023-06-28 23:01:22 +0200

    cleanups

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,38 +1,44 @@
-"use client"
-import React from 'react'
-import Link from 'next/link'
-import {CiLight, CiDark, CiSearch} from "react-icons/Ci";
-// import {IoIosPerson} from 'react-icons/Io';
-import {IoIosPersonAdd} from 'react-icons/Io';
+"use client";
+import React from "react";
+import Link from "next/link";
+import { CiLight, CiDark, CiSearch } from "react-icons/Ci";
+import { IoIosPerson } from "react-icons/Io";
 import { useTheme } from "next-themes";
 
 export default function Header() {
-    const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   return (
     <div className="relative border-b-4 border-slate-700 mb-10 z-1 ">
-        <div className="flex-start md:ml-2">
+      <div className="flex-start md:ml-2">
         <p>
-          <img src="https://devcircleafrica.com/wp-content/uploads/2022/07/cropped-logo-color.png" width={200} height={150} alt="logo"
+          <img
+            src="https://devcircleafrica.com/wp-content/uploads/2022/07/cropped-logo-color.png"
+            width={200}
+            height={150}
+            alt="logo"
           />
         </p>
+      </div>
+      <div className="flex grid-cols-3 gap-3 mt-3 ml-5 lg:mr-5 md:mr-3 absolute top-3 right-0  justify-end">
+        <div className="md:mr-2">
+          <Link href="/">
+            <IoIosPerson size={20} />
+          </Link>
         </div>
-        <div className="grid grid-cols-3 gap-3 mt-3 ml-5 lg:mr-5 md:mr-3 absolute top-3 right-0 flex justify-end">
-            <div className="md:mr-2">
-            <Link href="/">
-              <IoIosPerson size={20}/>
-            </Link>
-            </div>
-            <div>
-            <button className="md:mr-2" onClick={()=> setTheme( theme === "dark"? "light": "dark" )} >
-                { theme === "dark" ? <CiLight size={20}/>: <CiDark size={20}/> }
-            </button>
-            </div>
-            <div>
-            <Link href="/">
-              <CiSearch size={20}/>
-            </Link>
-            </div>
+        <div>
+          <button
+            className="md:mr-2"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {theme === "dark" ? <CiLight size={20} /> : <CiDark size={20} />}
+          </button>
+        </div>
+        <div>
+          <Link href="/">
+            <CiSearch size={20} />
+          </Link>
         </div>
       </div>
-  )
+    </div>
+  );
 }

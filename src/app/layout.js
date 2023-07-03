01431d03f6e5daf143da5ef9259394ff2415ab3b
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-07-03 23:32:23 +0300

    updates
    Implemented the dark mode, made the nav bar responsive on different screens, and added a footer. Had to import those icons back to react-icons instead of meronex, the others had problems.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,28 @@
-
 import './globals.css'
 import { Inter } from 'next/font/google';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Providers from './Providers.jsx';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'CMS Hub',
-  description: 'Developed to be used for content management system at DevCircle Africa',
-}
+  title: "Docs",
+  description: "at Docs, we are building the next generation of African developers",
+  keywords: "Programming, content writing, Designing, Learning, Hacks, Tech, Africa, Docs, Otofast Africa, Project, Africa Fast,"
+   
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+        <Header />
+        {children}
+        <Footer />
+        </Providers>
+      </body>
     </html>
   )
 }
